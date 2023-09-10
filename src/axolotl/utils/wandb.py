51517664ad3be2907c6c033e157c5de4febcc269
Hynek Kydlíček <hynek.kydlicek@blindspot.ai>
2023-09-10 23:01:46 +0200

    🔧run id

--- src/axolotl/utils/wandb.py
+++ src/axolotl/utils/wandb.py
@@ -1,6 +1,8 @@
 """Module for wandb utilities"""
 
 import os
+from pdb import run
+import uuid
 
 
 def setup_wandb_env_vars(cfg):
@@ -16,6 +18,15 @@ def setup_wandb_env_vars(cfg):
         if cfg.wandb_log_model and len(cfg.wandb_log_model) > 0:
             os.environ["WANDB_LOG_MODEL"] = cfg.wandb_log_model
         if cfg.wandb_run_id and len(cfg.wandb_run_id) > 0:
-            os.environ["WANDB_RUN_ID"] = cfg.wandb_run_id
+            run_id = cfg.wandb_run_id
+        else:
+            run_id = uuid.uuid4().hex
+
+        os.environ["WANDB_RUN_ID"] = run_id
+        # Setup output dir if not already set
+        if not cfg.output_dir:
+            cfg.output_dir = os.path.join(
+                "models",run_id
+            )
     else:
         os.environ["WANDB_DISABLED"] = "true"
